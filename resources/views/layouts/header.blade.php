d51a751f65c48661cf4ca5c959766e25a2b4ff30
John <125838389+pj301@users.noreply.github.com>
2024-10-10 18:59:54 +0800

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,3 @@
-
 <header id="header" class="header fixed-top d-flex align-items-center">
     <div class="d-flex align-items-center justify-content-between">
       <a href="{{ route('newsfeed') }}" class="logo d-flex align-items-center">
